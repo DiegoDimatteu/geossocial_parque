e9124112edb7c05add6b3edb0868664a2d9d53bf
Diego Dimatteu <diegodimatteu@gmail.com>
2022-05-09 21:19:35 -0300

    Atualização da home page

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -9,11 +9,37 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green[900],
-      body: Container(
-        child: ListView(
-          physics: const BouncingScrollPhysics(),
-          scrollDirection: Axis.horizontal,
+      appBar: AppBar(
+        toolbarHeight: 150,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Builder(
+          builder: (BuildContext context) {
+            return Padding(
+              padding: const EdgeInsets.only(left: 25, top: 40),
+              child: IconButton(
+                icon: const Icon(Icons.menu),
+                onPressed: () {
+                  Scaffold.of(context).openDrawer();
+                },
+                tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+              ),
+            );
+          },
+        ),
+      ),
+      drawer: Container(
+        child: FlutterLogo(),
+        width: MediaQuery.of(context).size.width * .6,
+        height: MediaQuery.of(context).size.height,
+        color: Colors.grey,
+      ),
+      backgroundColor: "004B23".toColor(),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(bottom: 100.0, left: 35, right: 35),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.end,
           children: const [
             ItenBotonMenu(
               icon: Icons.location_on_outlined,
--- lib/shared/widgets/iten_boton_menu.dart
+++ lib/shared/widgets/iten_boton_menu.dart
@@ -1,43 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:supercharged/supercharged.dart';
 
 class ItenBotonMenu extends StatelessWidget {
   final IconData icon;
   final String text;
+  final VoidCallback? clique;
 
-  const ItenBotonMenu({Key? key, required this.icon, required this.text})
+  const ItenBotonMenu(
+      {Key? key, required this.icon, required this.text, this.clique})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 35,
-      alignment: Alignment.center,
-      width: 90,
-      decoration: const BoxDecoration(
-        border: Border(
-          bottom: BorderSide(width: 0.7, color: Colors.white),
-          top: BorderSide(width: 0.7, color: Colors.white),
-        ),
-      ),
+    return SizedBox(
+      height: 100,
+      width: 100,
       child: ElevatedButton(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all("EBF2FA".toColor()),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(icon),
-                const SizedBox(
-                  width: 10,
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                Text(
-                  text,
-                  style: const TextStyle(fontSize: 10),
-                ),
-              ],
+            Icon(
+              icon,
+              color: "38B000".toColor(),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              text,
+              style: TextStyle(fontSize: 10, color: "38B000".toColor()),
             ),
           ],
         ),
